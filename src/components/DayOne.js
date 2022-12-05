4ee015a38b78d2b4dc4683293a9833de635a2638
ydaetwyler <ydaetwyler@previon.ch>
2022-12-05 22:35:12 +0100

    initial commit

---
+++ src/components/DayOne.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+
+const DayOne = () => {
+
+    return (
+        <div>
+            <h3 className="text-3xl text-yellow-500">Day {'>'} 1</h3>
+        </div>
+    )
+}
+
+export default DayOne
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => (
+    <div className="pt-8 flex flex-col items-center font-['Zen_Dots']">
+        <h1 className="text-6xl text-lime-400">Advent of Code 2022</h1>
+        <h2 className="mt-3 text-2xl text-lime-300">Yves Dätwyler</h2>
+    </div>
+)
+
+export default Header
